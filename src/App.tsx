b9bf45b640001899e6ac340edb2b21ffb872ab8f
Carlo Allan Laynes <carloallanlaynes@gmail.com>
2025-11-29 02:22:02 +0800

    feat(routing): setup react-router-dom architecture
    - Wrap application root in BrowserRouter in main.tsx.
    - Configure Route definitions in App.tsx nested under RootLayout.
    - Map all newly created page components to their specific URL paths.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootLayout from "@/layout";
-import HomePage from "@/pages/HomePage";
+import Home from "@/pages/HomePage";
+
+// Import new pages
+import OurFarm from "@/pages/OurFarmPage";
+import Shop from "@/pages/ShopPage";
+import LearnWithUs from "@/pages/LearnWithUsPage";
+import VisitUs from "@/pages/VisitUsPage";
+import OurPartners from "@/pages/OurPartnersPage";
+import Blog from "@/pages/BlogPage";
+import OurImpact from "@/pages/OurImpactPage";
+import JoinUs from "@/pages/JoinUsPage";
 
 function App() {
    return (
       <BrowserRouter>
          <Routes>
             <Route element={<RootLayout />}>
-               <Route path="/" element={<HomePage />} />
+               <Route path="/" element={<Home />} />
+
+               {/* Main Nav Items */}
+               <Route path="/farm" element={<OurFarm />} />
+               <Route path="/shop" element={<Shop />} />
+               <Route path="/learn" element={<LearnWithUs />} />
+               <Route path="/visit" element={<VisitUs />} />
+
+               {/* Dropdown Items */}
+               <Route path="/partners" element={<OurPartners />} />
+               <Route path="/blog" element={<Blog />} />
+               <Route path="/impact" element={<OurImpact />} />
+
+               {/* Action Items */}
+               <Route path="/join" element={<JoinUs />} />
             </Route>
          </Routes>
       </BrowserRouter>
