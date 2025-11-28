f2382b45106345ee3753dd2e26efe4976d07958f
Carlo Allan Laynes <carloallanlaynes@gmail.com>
2025-11-29 02:23:09 +0800

    fix(navbar): replace anchor tag with Link for logo routing

--- src/components/features/navbar/nav-logo.tsx
+++ src/components/features/navbar/nav-logo.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function NavLogo() {
    return (
-      <a href="/" className="flex items-center">
+      <Link to="/" className="flex items-center">
          <img
             src="/src/assets/images/logo/cruzalloma-logo.png"
             alt="Cruzalloma Farm Logo"
             className="h-13 md:h-14 w-auto object-contain"
          />
-      </a>
+      </Link>
    );
 }
