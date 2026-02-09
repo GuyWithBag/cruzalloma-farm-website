9b098d105b7be26643f6f8d222af5a542511f450
Carlo Laynes <carloallanlaynes@gmail.com>
2026-02-09 23:05:13 +0800

    feat: add modular components for hero header and featured crop

---
+++ src/components/features/hero-section/hero-header.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/ui/button';
+
+const HeroHeader = () => {
+  return (
+    <div className="max-w-2xl text-white">
+      {/* Title */}
+      <h1 className="text-heading-3 md:text-display-1 font-display font-black leading-[1.1] uppercase mb-3 sm:mb-6">
+        CULTIVATE <br /> & THRIVE
+      </h1>
+      
+      {/* Subtitle */}
+      <p className="text-[0.625rem] sm:text-xl lg:text-heading-4 font-display font-semibold mb-3 md:mb-5 lg:mb-10 md:max-w-115">
+        Your Partner In Building A Sustainable Agricultural Future For Mindanao.
+      </p>
+
+      {/* Primary CTA */}
+      <Button 
+        variant="default" 
+        size="lg" 
+        className="
+          text-[0.5rem] font-display font-bold 
+          px-2.5 py-1.5 sm:px-4 md:px-5 sm:py-3 
+          cursor-pointer
+          hover:brightness-105
+        "
+      >
+        EXPLORE OUR FARM
+      </Button>
+    </div>
+  );
+};
+
+export default HeroHeader;
