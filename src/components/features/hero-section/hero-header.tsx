ac3747c108c83644391d37e50fc2d018aa40c6ea
Carlo Laynes <carloallanlaynes@gmail.com>
2026-02-09 23:09:42 +0800

    refactor: reduce subtitle margin

--- src/components/features/hero-section/hero-header.tsx
+++ src/components/features/hero-section/hero-header.tsx
@@ -9,7 +9,7 @@ const HeroHeader = () => {
       </h1>
       
       {/* Subtitle */}
-      <p className="text-[0.625rem] sm:text-xl lg:text-heading-4 font-display font-semibold mb-3 md:mb-5 lg:mb-10 md:max-w-115">
+      <p className="text-[0.625rem] sm:text-xl lg:text-heading-4 font-display font-semibold mb-3 md:mb-5 lg:mb-9 md:max-w-115">
         Your Partner In Building A Sustainable Agricultural Future For Mindanao.
       </p>
 
