952ebe66196ea99c36edba2b79873fab78869ccf
LoejeeMan <ldulaugon123@gmail.com>
2026-03-28 13:28:21 +0800

    feat: add MarqueeSection to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,11 +3,13 @@ import FeaturesSection from "@/components/sections/FeaturesSection";
 import BenefitsSection from "@/components/sections/BenefitsSection";
 import HowItWorksSection from "@/components/sections/HowItWorksSection";
 import CTASection from "@/components/sections/CTASection";
+import MarqueeSection from "@/components/sections/MarqueeSection";
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
+      <MarqueeSection />
       <FeaturesSection />
       <BenefitsSection />
       <HowItWorksSection />
